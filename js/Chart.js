913bc0f32a3d97b9a44c51bdda997a11bde0289e
thanakrit <ketaauto55@gmail.com>
2023-09-07 23:33:49 +0700

    Update Chart

--- js/Chart.js
+++ js/Chart.js
@@ -1,56 +1,17 @@
 const ctx = document.getElementById('myChart');
-new Chart(ctx, {
+
+  const myChart =  new Chart(ctx, {
     type: 'line',
     data: {
       labels: ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00'],
       datasets: [{
-        label: 'Temperature',
+        // label: 'PM2.5',
         data: [12, 19, 3, 5, 2, 3],
         backgroundColor: "#f9b234",
         borderColor: "#f9b234",
-        borderWidth: 1,
+        borderWidth: 3,
         yAxisID: 'y1'
       },
-      {
-        label: 'Humidity',
-        data: [22, 9, 7, 5, 6, 13],
-        backgroundColor: "#3ecd5e",
-        borderColor: "#3ecd5e",
-        borderWidth: 1,
-        yAxisID: 'y2'
-      },
-      {
-        label: 'Air Pressure',
-        data: [47, 56, 10, 45, 34, 30],
-        backgroundColor: "#e44002",
-        borderColor: "#e44002",
-        borderWidth: 1,
-        yAxisID: 'y3'
-    },
-    {
-        label: 'Wind Speed',
-        data: [20, 19, 23, 25, 22, 33],
-        backgroundColor: "#952aff",
-        borderColor: "#952aff",
-        borderWidth: 1,
-        yAxisID: 'y4'
-      },
-      {
-        label: 'Wind Direction',
-        data: [33, 22, 35, 3, 23, 37],
-        backgroundColor: "#cd3e94",
-        borderColor: "#cd3e94",
-        borderWidth: 1,
-        yAxisID: 'y5'
-      },
-      {
-        label: 'Traffic',
-        data: [52, 15, 7, 4, 24, 8],
-        backgroundColor: "#4c49ea",
-        borderColor: "#4c49ea", 
-        borderWidth: 1,
-        yAxisID: 'y6'
-      }
     ]
     },
     options: {
@@ -59,11 +20,14 @@ new Chart(ctx, {
         mode: 'index',
         intersect: false,
       },
-      stacked: false,
+      stacked: true,
       plugins: {
         title: {
           display: true,
-          text: 'Chart Multi Value'
+          text: 'Chart PM2.5 Value'
+        },
+        legend:{
+          display: false,
         }
       },
       scales: {
@@ -72,31 +36,55 @@ new Chart(ctx, {
           display: true,
           position: 'left',
         },
-        y2: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-        },
-        y3: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-        },
-        y4: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-        },
-        y5: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-        },
-        y6: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-        },
       }
     },
   });
+
+
+function Chart_PM(){
+  myChart.options.plugins.title.text = "Chart PM2.5 Value";
+  myChart.data.datasets[0].data = [12, 19, 3, 5, 2, 3];
+  myChart.data.datasets[0].backgroundColor = "#f9b234";
+  myChart.data.datasets[0].borderColor = "#f9b234";
+  myChart.update();
+}
+
+function Chart_Temp(){
+  myChart.options.plugins.title.text = "Chart Temperature Value";
+  myChart.data.datasets[0].data = [52, 15, 7, 4, 24, 8];
+  myChart.data.datasets[0].backgroundColor = "#3ecd5e";
+  myChart.data.datasets[0].borderColor = "#3ecd5e";
+  myChart.update();
+}
+
+function Chart_Humid(){
+  myChart.options.plugins.title.text = "Chart Humidity Value";
+  myChart.data.datasets[0].data = [22, 9, 7, 5, 6, 13];
+  myChart.data.datasets[0].backgroundColor = "#e44002";
+  myChart.data.datasets[0].borderColor = "#e44002";
+  myChart.update();
+}
+
+function Chart_Pressure(){
+  myChart.options.plugins.title.text = "Chart Air Pressure Value";
+  myChart.data.datasets[0].data = [47, 56, 10, 45, 34, 30];
+  myChart.data.datasets[0].backgroundColor = "#952aff";
+  myChart.data.datasets[0].borderColor = "#952aff";
+  myChart.update();
+}
+
+function Chart_Speed(){
+  myChart.options.plugins.title.text = "Chart Wind Speed Value";
+  myChart.data.datasets[0].data = [20, 19, 23, 25, 22, 33];
+  myChart.data.datasets[0].backgroundColor = "#cd3e94";
+  myChart.data.datasets[0].borderColor = "#cd3e94";
+  myChart.update();
+}
+
+function Chart_Direction(){
+  myChart.options.plugins.title.text = "Chart Wind Direction Value";
+  myChart.data.datasets[0].data = [33, 22, 35, 3, 23, 37];
+  myChart.data.datasets[0].backgroundColor = "#4c49ea";
+  myChart.data.datasets[0].borderColor = "#4c49ea";
+  myChart.update();
+}
